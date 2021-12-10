168aa87b390cea7d78d7c1c7e84e704c051d20fe
Bryan Teixeira <bryanteixeiradc@gmail.com>
2021-12-09 23:32:43 -0400

    testing security configurations

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using BlogPostsManagementSystem.DataAccess;
 using BlogPostsManagementSystem.GraphQL;
 using BlogPostsManagementSystem.GraphQL.AuthorQL.Model;
@@ -9,20 +13,47 @@ using BlogPostsManagementSystem.GraphQL.BlogPostQL.Query;
 using BlogPostsManagementSystem.GraphQL.BlogPostQL.Repository;
 using HotChocolate.AspNetCore;
 using HotChocolate.AspNetCore.Playground;
+using HotChocolate.Execution;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BlogPostsManagementSystem
 {
+    public class CustomInterceptor : DefaultHttpRequestInterceptor
+    {
+        public override ValueTask OnCreateAsync(
+            HttpContext context,
+            IRequestExecutor requestExecutor,
+            IQueryRequestBuilder requestBuilder,
+            CancellationToken cancellationToken)
+        {
+            foreach (var key in context.Request.Headers)
+            {
+                Console.Write( key.Key, key.Value );
+            }
+            
+            requestBuilder.AddProperty("foo", context.Request.Headers);
+            return base.OnCreateAsync(context, requestExecutor, requestBuilder, cancellationToken);
+        }
+    }
+    
     public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json").Build();
+            
             services.AddDbContextFactory<ApplicationDbContext>(
                 options => options.UseInMemoryDatabase("BlogsManagement"));
             services.AddInMemorySubscriptions();
@@ -30,17 +61,40 @@ namespace BlogPostsManagementSystem
                 AuthorRepository>();
             services.AddScoped<IBlogPostRepository, 
                 BlogPostRepository>();
-            services
-                .AddGraphQLServer( "blogPost" )
-                .AddQueryType<BlogPostQuery>()
-                .AddType<BlogPostType>()
-                .AddSubscriptionType<Subscription>();
+            // services
+            //     .AddGraphQLServer("blogPost")
+            //     .AddQueryType<BlogPostQuery>()
+            //     .AddType<BlogPostType>()
+            //     //.AddSubscriptionType<Subscription>()
+            //     .AddHttpRequestInterceptor<CustomInterceptor>()
+            //     .AddAuthorization();
+            //
+            // services
+            //     .AddGraphQLServer("author")
+            //     .AddQueryType<AuthorQuery>()
+            //     .AddType<AuthorType>()
+            //     .AddMutationType<AuthorMutation>()
+            //     .AddType<BlogPostType>()
+            //     .AddHttpRequestInterceptor<CustomInterceptor>()
+            //     .AddAuthorization();
 
-            services
-                .AddGraphQLServer("author")
-                .AddQueryType<AuthorQuery>()
-                .AddType<AuthorType>()
-                .AddMutationType<AuthorMutation>();
+             services
+                 .AddGraphQLServer()
+                 .AddQueryType<Query>()
+                 .AddType<BlogPostType>()
+                 .AddType<AuthorType>()
+                 .AddMutationType<AuthorMutation>()
+                 .AddAuthorization();
+            
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(
+                options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                       ValidIssuer = config["JWT:Issuer"],
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,11 +109,14 @@ namespace BlogPostsManagementSystem
                     Path = "/playground"
                 });
             }
+            
+            app.UseAuthentication();
             app.UseWebSockets();
             app.UseRouting().UseEndpoints(endpoints =>
                 {
-                    endpoints.MapGraphQL("/blogPost", "blogPost");
-                    endpoints.MapGraphQL("/author", schemaName: "author");
+                    // endpoints.MapGraphQL("/blogPost", "blogPost");
+                    // endpoints.MapGraphQL("/author", schemaName: "author");
+                    endpoints.MapGraphQL();
                 });
         }
     }
